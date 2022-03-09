f332d83a6523719f8286523d04425eb84b8776b6
lmarino12 <lmarino@est.ups.edu.ec>
2022-03-08 19:22:28 -0500

    actualizacion al 100

---
+++ componentes/empleado/modelo.js
@@ -0,0 +1,22 @@
+const bcrypt =require('bcrypt-nodejs') 
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const reqString = {
+    type: String,
+    required: true,
+}
+
+const empleadoSchema = new Schema({
+    cedula: reqString,
+    nombre: reqString,
+    apellido: reqString,
+   // usuario: reqString,
+  //  clave: reqString
+})
+
+
+
+
+const model = mongoose.model('Empleado', empleadoSchema)
+module.exports = model
